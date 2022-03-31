67b40c672bc1abf6d223b4d41078559536284bd5
Nhật An <nhatan14042000@gmail.com>
2022-03-31 11:40:34 +0700

    Update

--- src/auth/auth.route.js
+++ src/auth/auth.route.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import passport from 'passport';
-import sendErrorsMessage from '../common/middlewares/sendErrorsMessage.middlware.js';
 import { handleLogin, handleRegister, getProfile } from './auth.controller.js';
-import loginSchema from './validators/login.validator.js';
 
 const authRouter = Router();
 
-authRouter.post('/login', loginSchema, sendErrorsMessage, handleLogin);
+authRouter.post('/login', handleLogin);
 
 authRouter.post('/register', handleRegister);
 
